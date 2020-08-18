2c660d48c2f65b837ceefcf01288e758e40d9e4f
vdebes <vdebes.externe@bedrockstreaming.com>
2020-08-18 11:58:37 +0200

    test quality decreases 2x once sell_in in 0

--- tests/GildedRoseTest.php
+++ tests/GildedRoseTest.php
@@ -6,6 +6,7 @@ namespace Tests;
 
 use GildedRose\GildedRose;
 use GildedRose\Item;
+use phpDocumentor\Reflection\Types\Void_;
 use PHPUnit\Framework\TestCase;
 
 class GildedRoseTest extends TestCase
@@ -16,6 +17,7 @@ class GildedRoseTest extends TestCase
         $value = 10;
         $item = new Item('foo', $value, 0);
         $this->assertObjectHasAttribute('sell_in', $item);
+
         $this->assertEquals($value, $item->sell_in);
     }
 
@@ -24,6 +26,7 @@ class GildedRoseTest extends TestCase
     {
         $quality = 10;
         $item = new Item('foo', 0, $quality);
+
         $this->assertObjectHasAttribute('quality', $item);
         $this->assertEquals($quality, $item->quality);
     }
@@ -33,10 +36,27 @@ class GildedRoseTest extends TestCase
     {
         $items = [new Item('foo', 1, 1), new Item('bar', 2, 2)];
         $gildedRose = new GildedRose($items);
+
         $gildedRose->updateQuality();
+
         $this->assertLessThan(1, $items[0]->sell_in);
         $this->assertLessThan(1, $items[0]->quality);
         $this->assertLessThan(2, $items[1]->sell_in);
         $this->assertLessThan(2, $items[1]->quality);
+    }
+
+    /** @testdox Once the sell by date has passed, Quality degrades twice as fast */
+    public function testPassedItemQualityDegradesTwiceAsFast(): Void
+    {
+        $initialQuality = 100;
+        $items = [new Item('foo', 1, $initialQuality)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $updatedQuality = $items[0]->quality;
+        $qualityDiff = $initialQuality - $updatedQuality;
+        $gildedRose->updateQuality();
+        $newQualityDiff = $updatedQuality - $items[0]->quality;
+
+        $this->assertEquals($newQualityDiff, $qualityDiff * 2);
     }
 }
