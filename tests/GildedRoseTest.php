bf329c5181518dff53c03fabab4b6e8fffe019ae
vdebes <vdebes.externe@bedrockstreaming.com>
2020-08-18 11:34:11 +0200

    test items properties decreases each day

--- tests/GildedRoseTest.php
+++ tests/GildedRoseTest.php
@@ -27,4 +27,16 @@ class GildedRoseTest extends TestCase
         $this->assertObjectHasAttribute('quality', $item);
         $this->assertEquals($quality, $item->quality);
     }
+
+    /** @testdox At the end of each day our system lowers both values for every item */
+    public function testItemsQualityDecreases(): void
+    {
+        $items = [new Item('foo', 1, 1), new Item('bar', 2, 2)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertLessThan(1, $items[0]->sell_in);
+        $this->assertLessThan(1, $items[0]->quality);
+        $this->assertLessThan(2, $items[1]->sell_in);
+        $this->assertLessThan(2, $items[1]->quality);
+    }
 }
